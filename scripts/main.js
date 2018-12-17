7243df92fe15507ae01b78f674d4f78094d7b7d7
Bodyhealer <klodium@gmail.com>
2018-12-17 23:23:04 +0500

    Test site. HTML. CSS. JavaScript.

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === "images/logo.png"){
+        myImage.setAttribute('src', 'images/bull.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/logo.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Пожалуйста введите своё имя.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla крутая, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla крутая, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
