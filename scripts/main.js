a0b793dce21258a55194158c7284bbb20bd8b76b
Алексей Галимов Наилевич <galimov_an@chkz.ru>
2018-12-26 11:26:14 +0500

    list adding changin script

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
 var myImage = document.querySelector('img');
 
-myImage.onclick = function(){
+myImage.onclick = function (e2) {
+    e2.stopPropagation;
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === "images/logo.png"){
+    if (mySrc === "images/logo.png") {
         myImage.setAttribute('src', 'images/bull.jpg');
     } else {
         myImage.setAttribute('src', 'images/logo.png');
     }
-}
+};
 
-var myButton = document.querySelector('button');
+var myButton = document.querySelector('button.change-name');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
@@ -27,4 +28,38 @@ if (!localStorage.getItem('name')) {
 
 myButton.onclick = function () {
     setUserName();
+};
+
+var list = document.querySelector('ul.what_do');
+var btnAddRow = document.querySelector('button.add-row');
+
+list.querySelectorAll('li').forEach(e => {e.addEventListener('click', function(e1) {
+        e1.stopPropagation();
+        var listContent = prompt("Введи новый текст для строки из списка");
+        this.textContent = listContent;
+    })});
+
+/*document.querySelectorAll('li').forEach(e => {e.addEventListener('click', function(e1) {
+        e1.stopPropagation();
+        var listContent = prompt("Введи новый текст для строки из списка");
+        this.textContent = listContent;
+    })});*/
+
+/*list.childNodes.forEach(e => {e.addEventListener('click', function(e1) {
+        e1.stopPropagation();
+        var listContent = prompt("Введи новый текст для строки из списка");
+        this.textContent = listContent;
+    })});*/
+
+btnAddRow.onclick = function () {
+    var listItem = document.createElement('li');
+    var listContent = prompt("Какой текст ты хочешь добавить в список?");
+    listItem.textContent = listContent;
+    list.appendChild(listItem);
+    
+    listItem.onclick = function(e) {
+        e.stopPropagation();
+        var listContent = prompt("Введи новый текст для строки из списка");
+        this.textContent = listContent;
+    }
 }
