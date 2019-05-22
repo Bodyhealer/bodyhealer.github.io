f366b5b723f2c8383d54a041d9484168fb689684
Bodyhealer <myp3uk2005@yandex.ru>
2019-05-22 15:09:44 +0500

    import/export in modules

--- scripts/modules/bodyMath.js
+++ scripts/modules/bodyMath.js
@@ -17,7 +17,7 @@ const factorial_while = (n) => {
     return result;
 }
 
-const factorial_for = (n) => {
+export const factorial_for = (n) => {
     let result = 1;
 
     for (let i = 1; i <= n; i++) {
